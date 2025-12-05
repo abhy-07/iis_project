1408d844d4276346ae76a9699227716bc1bb0e3b
Molly <molly.borjes.9732@student.uu.se>
2025-12-05 12:48:22 +0100

    removed kotlin files

--- src/story_engine/StoryRules.kt
+++
@@ -1,5 +0,0 @@
-# placeholder functions:
-
-fun selectStory(lastEmotion: String, mood: String): String { ... }
-fun pickSceneTemplate(scene: Scene, emotion: String): String { ... }
-fun nextScene(currentId: String, intent: Intent, emotion: String): String { ... }
